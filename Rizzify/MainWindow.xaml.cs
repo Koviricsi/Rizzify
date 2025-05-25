37189c36ab0b2cb77383560e87040341e23a063a
Kovács Bercike Sigma <fazekasbertalan2008@gmail.com>
2025-05-25 13:45:09 +0200

    sex

--- Rizzify/MainWindow.xaml.cs
+++ Rizzify/MainWindow.xaml.cs
@@ -1,25 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Rizzify
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> mp3List = new List<string>();
+        private bool isFavoritesVisible;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +20,8 @@ namespace Rizzify
 
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            this.DragMove();
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
         }
 
         private void btClose_Click(object sender, RoutedEventArgs e)
@@ -37,14 +31,50 @@ namespace Rizzify
 
         private void btMaxMin_Click(object sender, RoutedEventArgs e)
         {
-            if (WindowState == WindowState.Maximized)
+            this.WindowState = this.WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
+
+        private void btFavorites_Click(object sender, RoutedEventArgs e)
+        {
+            isFavoritesVisible = !isFavoritesVisible;
+
+            
+            btFavorites.Content = isFavoritesVisible ? "♥" : "♡";
+
+            if (isFavoritesVisible)
             {
-                WindowState = WindowState.Normal;
+                try
+                {
+                    string folderPath = @"C:\Zene";
+
+                    if (Directory.Exists(folderPath))
+                    {
+                        string[] files = Directory.GetFiles(folderPath, "*.mp3");
+                        mp3List = files.Select(f => Path.GetFileName(f)).ToList();
+
+                        MusicListBox.ItemsSource = null;
+                        MusicListBox.ItemsSource = mp3List;
+                        MusicListBox.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"A mappa nem található: {folderPath}", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Hiba történt a fájlok betöltésekor:\n{ex.Message}", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
             else
             {
-                WindowState = WindowState.Maximized;
+                MusicListBox.Visibility = Visibility.Collapsed;
             }
         }
+
+
     }
 }
+
